499fb3ea262b8d8b20030ee194730ca323b02417
Nur Özer <nbasakozer@gmail.com>
2024-02-20 04:02:18 +0100

    TODO: Duplicate sentences for each predicate

--- A2/preprocessing.py
+++ A2/preprocessing.py
@@ -17,6 +17,39 @@ def preprocessing(input_filepath, output_filepath):
                 if not line:
                     sentence_id += 1  # Increment sentence ID for the next sentence
 
+    with open(output_filepath, encoding='utf-8') as file:
+        lines = file.readlines()
+
+    data = [line.strip().split('\t') for line in lines]
+    max_columns = max(len(row) for row in data)
+    print(max_columns)
+    new_df = pd.DataFrame(data, columns=[f'col_{i}' for i in range(max_columns)])
+    
+    # Rename only the first 11 columns (sentence_num + 10 other - see assignment guidelines)
+    additional_cols = [f'col_{i}' for i in range(11, max_columns)]
+    new_df.columns = ['sentence_num', 'token', 'lemma', 'UPOS', 'POS', 'grammar', 'head_id', 'dependency_label',
+                      'head_dependency_relation', 'additional_info', 'is_predicate'] + additional_cols
+
+    ##################################################################################################################
+    # TODO: To my understanding, all of these additional columns means there are sentences with up to 35 predicates. #
+    # We need to duplicate the sentences for each predicate and then drop the column with no unique name.            #
+    ##################################################################################################################
+    
+    # Get a list of column names starting with 'Column_' -> didn't work as intended
+    columns_to_drop = []
+
+    # Iterate through columns
+    for column in new_df.columns:
+        if column.startswith('col_'):
+            # Check if any non-empty value exists in the column
+            if new_df[column].notna().any():
+                continue  # Keep the column
+            else:
+                columns_to_drop.append(column)  # Mark for dropping
+    
+    return new_df        
+
 if __name__ == '__main__':
     
-    preprocessing('A2/data/en_ewt-up-train.conllu', 'A2/data/en_ewt-up-train_new.conllu')
+    df = preprocessing('A2/data/en_ewt-up-train.conllu', 'A2/data/en_ewt-up-train_new.conllu')
+    df.to_csv('A2/data/en_ewt-up-train_new.csv', index=False)
