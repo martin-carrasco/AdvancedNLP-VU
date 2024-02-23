f777de97682da15124424bb8eae20761c58d989f
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-02-23 08:06:58 +0100

    FIX: Model inputs

--- A2/model_class.py
+++ A2/model_class.py
@@ -1,27 +1,28 @@
 from sklearn.linear_model import LogisticRegression
+from joblib import dump, load
 
 import pandas as pd
-import pickle
 
 from utils import feature_encode
 
 def train_model(train_file: str):
     """  Train the model and save it to a file
     """
-    df = pd.read_csv(train_file, delimiter='\t')
+    df = pd.read_csv('data/input/' + train_file, delimiter='\t')
     df = df[(df['label'] != 'O') | (df['label'] != 'V')]
     X, y = feature_encode(df)
     clf = LogisticRegression(random_state=0, max_iter=1000, multi_class='multinomial').fit(X, y)
-    pickle.dumps(clf, open('data/models/model_classf.pkl', 'wb'))
+    dump(clf, 'data/models/model_classf.joblib')
+    print('Model saved to model_classf.joblib')
 
 def predict_model(test_file: str, model_file: str):
     """  Predict the model and save it to a file
     """
-    df = pd.read_csv(test_file)
+    df = pd.read_csv('data/models/' + test_file, delimiter='\t')
     df = df[(df['label'] != 'O') | (df['label'] != 'V')]
     X, y = feature_encode(df)
 
-    clf = pickle.load(open(f'data/models/{model_file}', 'rb'))
+    clf = load(f'data/models/{model_file}')
     y_pred = clf.predict(X)
     df=pd.DataFrame({'y': y, 'pred_y': y_pred})
     df.to_csv('data/preds/classf.csv', index=False)
--- A2/model_iden.py
+++ A2/model_iden.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-import pickle
+from joblib import dump, load
 import pandas as pd
 
 from utils import feature_encode
@@ -8,16 +8,17 @@ from utils import feature_encode
 def train_model(train_file: str):
     """  Train the model and save it to a file
     """
-    df = pd.read_csv(train_file, delimiter='\t')
+    df = pd.read_csv('data/input/' + train_file, delimiter='\t')
     X, y = feature_encode(df)
     clf = LogisticRegression(random_state=0, max_iter=1000).fit(X, y)
-    pickle.dumps(clf, open('data/models/model_ident.pkl', 'wb'))
-    print('Model saved to model_ident.pkl')
+    dump(clf, 'data/models/model_ident.joblib')
+    print('Model saved to model_ident.joblib')
 
 
 def predict_model(test_file: str, model_file: str):
-    X, y = feature_encode(test_file)
-    clf = pickle.loads(open(f'data/models/{model_file}', 'rb'))
+    df = pd.read_csv('data/input/' + test_file, delimiter='\t')
+    X, y = feature_encode(df)
+    clf = load(f'data/models/{model_file}')
     y_pred = clf.predict(X)
     df=pd.DataFrame({'y': y, 'pred_y': y_pred})
     df.to_csv('data/preds/ident.csv', index=False)
