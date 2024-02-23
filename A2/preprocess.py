5e216f182f6ffba178542461e17e21fe39ad89fe
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-02-23 07:29:25 +0100

    ADD: Main preprocess file

--- A2/preprocess.py
+++ A2/preprocess.py
@@ -1,23 +1,14 @@
-from sklearn.linear_model import LogisticRegression
-from conllu import parse
 import pandas as pd
 
 
-
-conll_dir = 'data/'
-test_file = conll_dir + 'en_ewt-up-test.conllu' 
-train_file = conll_dir + 'en_ewt-up-train.conllu' 
-dev_file = conll_dir + 'en_ewt-up-dev.conllu' 
-
-
-
+raw_dir = 'data/raw/'
 df_dict = {
     'token_id': [],
     'sentence_num': [],
-    'word': [],
+    'token': [],
     'lemma': [],
     'upos': [],
-    'xpos': [],
+    'POS': [],
     'feats': [],
     'head': [],
     'deprel': [],
@@ -27,77 +18,104 @@ df_dict = {
     'up:args': []
 }
 
-with open(dev_file, 'r') as f:
-    sent_cnt = -1
-    for line in f.readlines():
-        line = line.strip()
-        if line.startswith('#'):
-            continue
-        columns = line.split('\t')
-        if len(columns) < 2:
-            continue
 
-        token_id = columns[0]
-        if token_id == '1':
-            sent_cnt+=1
-        df_dict['sentence_num'].append(sent_cnt)
-        df_dict['token_id'].append(columns[0])
-        df_dict['word'].append(columns[1])
-        df_dict['lemma'].append(columns[2])
-        df_dict['upos'].append(columns[3])
-        df_dict['xpos'].append(columns[4])
-        df_dict['feats'].append(columns[5])
-        df_dict['head'].append(columns[6])
-        df_dict['deprel'].append(columns[7])
-        df_dict['deps'].append(columns[8])
-        df_dict['misc'].append(columns[9])
-        if len(columns) > 10:
-            df_dict['up:preds'].append(columns[10])
-            df_dict['up:args'].append(columns[11:])
-        else:
-            df_dict['up:preds'].append('_')
-            df_dict['up:args'].append(['_'])
-df = pd.DataFrame(df_dict)
+def check_label(x, i: int) -> str:
+    if len(x) < i+1:
+        return 'O'
+    else:
+        return x[i] if x[i] != '_' else 'O'
 
+def parse(filename: str) -> pd.DataFrame:
+    """ Parse a conllu file and return a dataframe with the parsed data
+    """
+    with open(raw_dir + filename, 'r') as f:
+        sent_cnt = -1
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith('#'):
+                continue
+            columns = line.split('\t')
+            if len(columns) < 2:
+                continue
 
-def preprocessing(df):
-    token_df_dict = {
-        'token': [],
-        'lemma': [],
-        'pos': [],
-        'predicate': [],
-        'srl': [],
-        'deprel': []
-    }
+            token_id = columns[0]
+            if token_id == '1':
+                sent_cnt+=1
+            df_dict['sentence_num'].append(sent_cnt)
+            df_dict['token_id'].append(columns[0])
+            df_dict['token'].append(columns[1])
+            df_dict['lemma'].append(columns[2])
+            df_dict['upos'].append(columns[3])
+            df_dict['POS'].append(columns[4])
+            df_dict['feats'].append(columns[5])
+            df_dict['head'].append(columns[6])
+            df_dict['deprel'].append(columns[7])
+            df_dict['deps'].append(columns[8])
+            df_dict['misc'].append(columns[9])
+            if len(columns) > 10:
+                df_dict['up:preds'].append(columns[10])
+                df_dict['up:args'].append(columns[11:])
+            else:
+                df_dict['up:preds'].append('_')
+                df_dict['up:args'].append(['_'])
+    return pd.DataFrame(df_dict)
+
+def preprocessing(filename: str) -> pd.DataFrame:
+    """  Preprocess the conllu file and return a dataframe with the preprocessed data
+    """
+    df = parse(filename)
+
+    df_list = []
+
+    # Get unique sentence ids
     sentence_ids = df['sentence_num'].unique()
+
+    # Initialize columns
+    df['sentence_num_clone'] = 0
+    df['predicate'] = df['up:preds']
+    new_sentence_id = df['sentence_num'].max() + 1
+
     # For each senttence
     for sentence_id in sentence_ids:
         sentence_df = df[df['sentence_num'] == sentence_id]
+
         # Extract the predicates of this sentence
-        predicates = list(sentence_df['up:preds'].unique())
-        # Remove empty predicate
+        predicates = []
+        predicate_tokens = []
+        predicate_token_ids = []
+
+        # Extract the actual tokens of the predicates
+        for i, row in sentence_df.iterrows():
+            if row['up:preds'] == '_':
+                continue
+            predicate_tokens.append(row['token'])
+            predicates.append(row['up:preds'])
+            predicate_token_ids.append(row['token_id'])
+
+
+         # _ is not a prediate
         if '_' in predicates:
             predicates.remove('_')
-        
-        for pred in predicates:
-            # Iterate over the tokens which are not predicates
-            for i, row in sentence_df[sentence_df['up:preds'] == '_'].iterrows():
-                if row['upos'] in ['PUNCT', 'NUM', 'PART', 'SYM', 'X']:
-                    continue
-                token_df_dict['token'].append(row['word'])
-                token_df_dict['lemma'].append(row['lemma'])
-                token_df_dict['pos'].append(row['upos'])
-                token_df_dict['deprel'].append(row['deprel'])
 
+        # Create a new sentence based on predicate
+        for i, tup in enumerate(zip(predicates, predicate_tokens, predicate_token_ids)):
+            pred, pred_token, pred_token_id = tup
+            new_sentence_df = sentence_df.copy()
+            new_sentence_df['sentence_num_clone'] = new_sentence_id if i != 0 else sentence_id
+            new_sentence_df['predicate'] = pred
+            new_sentence_df['predicate_token'] = pred_token
+            new_sentence_df['predicate_token_id'] = pred_token_id
+            new_sentence_df['label'] = new_sentence_df['up:args'].apply(lambda x: check_label(x, i))
 
-                if all(p=='_'for p in row['up:args']):
-                    token_df_dict['predicate'].append(False)
-                    token_df_dict['srl'].append(False)
-                else:
-                    token_df_dict['predicate'].append(pred)
-                    token_df_dict['srl'].append(row['up:args'][predicates.index(pred)])
-    return pd.DataFrame(token_df_dict)
+            # Add DF subsection
+            df_list.append(new_sentence_df)
 
-n_df = preprocessing(df)
+            if i != 0:
+                new_sentence_id += 1
+    new_df = pd.concat(df_list)
+    new_df['sentence_id'] = new_df['sentence_num_clone']
+    new_df = new_df.drop(['sentence_num_clone'], axis=1)
 
-n_df.to_csv('data/dev.csv', index=False)
+    file_name = filename.split('.')[0]
+    new_df.to_csv(f'data/preprocessed/{file_name}_pp.tsv', index=False, sep='\t')
+    return new_df
