e155df6e654c146ab7a30c58d5b03408a58823f8
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-03-05 09:27:54 +0100

    FIX: utils

--- A3/utils.py
+++ A3/utils.py
@@ -20,30 +20,25 @@ def tokenize_and_align_labels(tokenizer, row):
         tokenized_inputs['token_type_ids'].append(pred_tokens["token_type_ids"][idx])
         tokenized_inputs['attention_mask'].append(pred_tokens["attention_mask"][idx])
 
-    labels = []
-    for i, label in enumerate(row['label']):
-        word_ids = tokenized_inputs.word_ids(batch_index=i)
-        print(word_ids)
-        previous_word_idx = None
-        label_ids = []
-        for word_idx in word_ids:
-            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-            # ignored in the loss function.
-            if word_idx is None:
-                label_ids.append(-100)
-            # We set the label for the first token of each word.
-            elif word_idx != previous_word_idx:
-                print(label)
-                label_ids.append(label[word_idx])
-            # For the other tokens in a word, we set the label to either the current label or -100, depending on
-            # the label_all_tokens flag.
-            else:
-                label_ids.append(label[word_idx] if label_all_tokens else -100)
-            previous_word_idx = word_idx
+    word_ids = tokenized_inputs.word_ids()
+    previous_word_idx = None
+    label_ids = []
+    for word_idx in word_ids:
+        # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+        # ignored in the loss function.
+        if word_idx is None:
+            label_ids.append(-100)
+        # We set the label for the first token of each word.
+        elif word_idx != previous_word_idx:
+            label_ids.append(row['label'][word_idx])
+        # For the other tokens in a word, we set the label to either the current label or -100, depending on
+        # the label_all_tokens flag.
+        else:
+            label_ids.append(row['label'][word_idx] if label_all_tokens else -100)
+        previous_word_idx = word_idx
 
-        labels.append(label_ids)
 
-    tokenized_inputs["labels"] = labels
+    tokenized_inputs["labels"] = label_ids
     return tokenized_inputs
 
 def compute_metrics(p, label_list):
