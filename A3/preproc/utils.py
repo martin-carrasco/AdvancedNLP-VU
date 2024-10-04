FIXL: Add correct prediction by averaging distributions and token_type_ids

A3/preproc/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 import numpy as np
 from typing import List, Dict
 
@@ -25,7 +26,7 @@ def tokenize_and_align(tokenizer, row):
     pred_token = row['pred'][0]
     pred_token_base = row['pred_base'][0]
     tok_sent = tokenizer(row["token"], is_split_into_words=True)
-    tok_whole = tokenizer(row["token"], [pred_token, pred_token_base], padding='max_length', max_length=64, truncation=True, is_split_into_words=True)
+    tok_whole = tokenizer(row["token"], [pred_token], padding='max_length', max_length=64, truncation=True, is_split_into_words=True)
 
     label_ids = []
     pred_idx = row['is_pred'].index(True)
@@ -39,26 +40,35 @@ def tokenize_and_align(tokenizer, row):
             # If the token is part of the predicate do not add a label
             label_ids.append(-100)
         else: 
-            if wordpiece[i].startswith("##"):
-                label_ids.append(-100)
-                continue
             # Set the label of the first token of each word
             label_ids.append(row['label'][word_idx])
 
-    token_type_ids = [0] * len(tok_whole['input_ids'])
+    token_type_ids = torch.zeros(len(tok_whole['input_ids']))
+    scatter_idx = []
+    idx_cnt = -1
 
     # Add token_type_ids
     for i, word_idx in enumerate(tok_whole.word_ids()):
         # If it is a special token do not add a label
         if word_idx is None:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
             continue
         elif wordpiece[i].startswith("##"):
+            scatter_idx.append(idx_cnt)
             continue
         elif word_idx == pred_idx:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
             token_type_ids[i] = 1
-            break
+            continue
+        else:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+
     tok_whole["token_type_ids"] = token_type_ids
     tok_whole["labels"] = label_ids
+    tok_whole["scatter_idx"] = torch.tensor(scatter_idx)
     return tok_whole 
 
 def compute_metrics(p, label_list):