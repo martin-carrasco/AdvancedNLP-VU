36f7cc9d79175c5736124d285c5da317e6da138a
Martin Carrasco <martin.carrasco@utec.edu.pe>
2024-10-07 18:45:52 +0200

    FIX: Exam code

--- Exam/generate_dataset.py
+++ Exam/generate_dataset.py
@@ -1,10 +1,10 @@
 import random
 import json
-import pipelines
 import itertools
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 from transformers import pipeline
 import numpy as np
+import pdb
 
 import checklist
 from checklist.editor import Editor
@@ -43,26 +43,8 @@ def add_goal(x, nsamples=10):
 
     return rets
 
-def add_source(x, nsamples=10):
-    phrases = ['from the store', 'from my window', 'from my neighbourhood', 'from jail']
-    cnt = 0
-    rets = []
-    while cnt < nsamples:
-        x_c = random.choice(x)
-        pred_pos = x_c[1]
-        arg_pos = x_c[2]
-        label = x_c[3]
-        pred_frame = x_c[4]
-        x_c = x_c[0]
-        p = random.choice(phrases)
-        data_tup = ('{} {}'.format(x_c, p), pred_pos, arg_pos, label, pred_frame)
-        rets.append(data_tup)
-        cnt+=1
 
-    return rets
-
-
-def test_1_CA0_R():
+def test_1_PatientRight():
     # Generated using templates
     dataset = [
         ('There was a book', 'be.02'),
@@ -97,62 +79,73 @@ def test_1_CA0_R():
 
     data_tup = [(x[0], pred_pos, arg_pos, label, x[1]) for x in dataset]
 
-    test = MFT(data=data_tup, labels=arg_pos, name='CA0-R - T1', capability='CA0-R', description='X')
+    test = MFT(data=data_tup, labels=arg_pos, name='PatientRight', capability='PatientRight', description='Patient(ARG1) is to the write of the predicate')
 
     return test
 
-def test_1_CA1_D():
-    adjs = ['well', 'badly', 'quietly']
-    prop = ['on', 'in', 'under', 'beside']
-    nouns = ['person', 'child', 'president', 'doctor', 'cat', 'teacher']
-    data = editor.template('The {nouns} {prop} {mask} {mask} is {adjs}', prop=prop, nouns=nouns, adjs=adjs, save=True, labels=5, nsamples=25)
+def test_1_PatientRole3D():
+    nouns = ['person', 'child', 'animal', 'mailman']
+    prop = ['aside', 'beside', 'by', 'near']
+    add = ['that', 'this']
+    nouns_two = ['car', 'house', 'road']
+    adjs = ['green', 'blurry', 'sad', 'happy', 'angry']
+    data = editor.template('The {nouns} {prop} {add} {nouns_two} is {adjs}',
+                            prop=prop, nouns=nouns, adjs=adjs, nouns_two=nouns_two,
+                            add=add,
+                            save=True, labels=5, nsamples=25)
 
     pred_pos = 5
     arg_pos = 1
-    label='ARG0'
+    label='ARG1'
 
     data_tup = [(x, pred_pos, arg_pos, label, 'be.02') for x in data.data]
 
-    test = MFT(data=data_tup, labels=arg_pos, name='CA1-D - T1', capability='CA1-D', description='X')
+    test = MFT(data=data_tup, labels=arg_pos, name='PatientRole3D', capability='PatientRole3D', description='Patient(ARG1) has a distance of 3 or more to the predicate')
     return test
 
-def test_1_CA0_D():
-    ors = ['or', 'and', 'but', 'yet']
-    verbs = ['uses', 'eats', 'drinks', 'watches', 'cries']
-    verbs_frame =['use.01', 'eat.01', 'drink.01', 'watch.01', 'cry.01']
-    en = ['that', 'which', 'who', 'whom']
-    prep = ['Everybody', 'Nobody', 'Somebody', 'Anybody']
-    adverb_a = ['swiftly', 'slowsly', 'inaudibly', 'inadvertedly', 'unintentionally']
-    adverb_b = ['excitedly', 'happily', 'sadly', 'angrily', 'calmly']
-    data = editor.template('{prep} {en} {adverb_a} {ors} {adverb_b} {verbs} {mask}', ors=ors, adverb_a=adverb_a, adverb_b=adverb_b, en=en, verbs=verbs, prep=prep, nsamples=25)
+def test_1_AgentRole3D():
+    prep = ['Nobody', 'Anybody']
+    en = ['that']
+    adverb_a = ['unintentionally', 'accidentally', 'intentionally']
+    ors = ['and', 'yet']
+    adverb_b = ['happily', 'sadly', 'angrily', 'calmly']
+    verbs = ['eats', 'drinks', 'cries', 'laughs']
+    filler = ['in public', 'in private', 'in the bus']
+    verbs_two = ['is']
+    adj = ['trustworthy', 'unreliable', 'smart']
+    data = editor.template('{prep} {en} {adverb_a} {ors} {adverb_b} {verbs} {filler} {verbs_two} {adj}',
+                            ors=ors, adverb_a=adverb_a, adverb_b=adverb_b, en=en, verbs=verbs,
+                            filler=filler, verbs_two=verbs_two, adj=adj,
+                            prep=prep, nsamples=25)
 
     pred_pos = 5
-    arg_pos = 1
-    label = 'ARG1'
+    arg_pos = 0
+    label = 'ARG0'
+    verbs_frame =['eat.01', 'drink.01', 'watch.01', 'cry.01']
 
     data_tup = []
     for x in data.data:
         idx = verbs.index(x.split(' ')[5])
         data_tup.append((x, pred_pos, arg_pos, label, verbs_frame[idx]))
 
-    test = MFT(data=data_tup, labels=arg_pos, name='CA0-D - T1', capability='CA0-D', description='X')
+    test = MFT(data=data_tup, labels=arg_pos, name='AgentRole3D', capability='AgentRole3D', description='Agent(ARG0) has a distance of 3 or more to the predicate')
     return test
     
-def test_1_CA_LOC_L():
+def test_1_LocationVar():
     label = 'ARGM-LOC'
     dataset = [
         ('I bought flowers in Miami', 1, 4, label, 'buy.01'),
         ('My mother used to live in Mexico', 4, 6, label, 'live.01'),
         ('There is a fantastic flower shop in China', 1, 7, label, 'be.02')
     ]
-    t = Perturb.perturb(dataset, change_loc, nsamples=25, keep_original=False)
+    t = Perturb.perturb(dataset, change_loc, nsamples=30, keep_original=False)
     data_tup = t.data
 
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-LOC-L - T1', capability='CA-LOC-L', description='X')
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='LocationVar', capability='LocationVar', description='X')
     return test
 
-def test_1_CA_NEG_A():
-    na = ['never', 'hardly', 'rarely', 'seldom', 'barely']
+def test_1_NegAdv():
+    na = ['never']
     sents = [
         ('I {na} go to the gym', 'go.01'),
         ('I {na} eat meat', 'eat.01'),
@@ -175,14 +168,14 @@ def test_1_CA_NEG_A():
             sent =  s[0].format(na=n_a) 
             data_tup.append((sent, pred_pos, arg_pos, label, s[1]))
     
-    test = MFT(data=data_tup, labels=arg_pos, name='CA-NEG-A - T1', capability='CA-NEG-A', description='X')
+    test = MFT(data=data_tup, labels=arg_pos, name='NegAdv', capability='NegAdv', description='X')
     return test
 
-def test_1_CA_TMP_D():
+def test_1_TempAdv():
     label = 'ARGM-TMP'
     dataset = [
-        ('My brithday is on Monday', 2, 4, label, 'be.02'),
-        ('I visited my grandmother last May', 2, 5, label, 'visit.01'),
+        ('My brithday will take place on Monday', 3, 6, label, 'take.01'),
+        ('I visited my grandmother last May', 1, 5, label, 'visit.01'),
         ('I will have a party next Saturday', 2 , 6, label, 'have.04')
     ]
 
@@ -191,41 +184,41 @@ def test_1_CA_TMP_D():
     data_tup = []
     for x in data_tup_raw:
         for y in x:
-            data_tup.append((y[0], y[1], y[2], label, x[4]))
+            data_tup.append((y[0], y[1], y[2], label, y[4]))
 
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-TMP-D - T1', capability='CA-TMP-D', description='X')
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='TempAdv', capability='TempAdv', description='X')
     return test
 
-def test_1_CA_TMP_F():
+def test_1_FreqAdvPast():
     label = 'ARGM-TMP'
     sentences_past = [
-        ('I visited my grandmother recently', 2, 4, label, 'visit.01'),
-        ('I ate a cake yesterday', 2, 4, label, 'eat.01'),
-        ('He slept last night', 2, 3, label, 'sleep.01'),
-        ('They went to the beach last week', 2, 6, label, 'go.01'),
-        ('She cared for her dog last month', 2, 6, label, 'care.03')
+        ('I visited my grandmother recently', 1, 4, label, 'visit.01'),
+        ('I ate a cake yesterday', 1, 4, label, 'eat.01'),
+        ('He slept last night', 1, 3, label, 'sleep.01'),
+        ('They went to the beach last week', 1, 6, label, 'go.01'),
+        ('She cared for her dog last month', 1, 6, label, 'care.03')
     ]
 
     t = Perturb.perturb(sentences_past, perturb_time_past, nsamples=50)
     data_tup = t.data
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-TMP-F - T1', capability='CA-TMP-F', description='X')
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='FreqAdvPast', capability='FreqAdvPast', description='X')
     return test
 
-def test_2_CA_TMP_F():
+def test_2_FreqAdvPresent():
     label = 'ARGM-TMP'
     sentences_present = [
-        ('My brithday is tomorrow', 2, 3, label, 'be.02'),
-        ('Her car arrives today', 2, 3, label, 'arrive.01'),
-        ('The party starts this afternoon', 2, 4, label, 'start.01'),
+        ('I will be having my birthday tomorrow', 3, 6, label, 'have.02'),
+        ('Her car is arriving today', 3, 4, label, 'arrive.01'),
+        ('The party is starting this afternoon', 3, 5, label, 'start.01'),
         ('The concert begins tonight', 2, 3, label, 'begin.01'),
         ('The movie premiers this evening' , 2, 4, label, 'premier.01')
     ]
     t = Perturb.perturb(sentences_present, perturb_time_present, nsamples=50)
     data_tup = t.data
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-TMP-F - T2', capability='CA-TMP-F', description='X')
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='FreqAdvPresent', capability='FreqAdvPresent', description='X')
     return test
 
-def test_3_CA_TMP_F():
+def test_3_FreqAdvFuture():
     label = 'ARGM-TMP'
     sentences_future = [
         ('I will have a party tonight', 2, 5, label, 'have.04'),
@@ -238,56 +231,98 @@ def test_3_CA_TMP_F():
     t = Perturb.perturb(sentences_future, perturb_time_future, nsamples=25)
     data_tup = t.data
 
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-TMP-F - T3', capability='CA-TMP-F', description='X')
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='FreqAdvFuture', capability='FreqAdvFuture', description='X')
     return test
 
-def test_1_CA_DIR_P():
+def test_1_VarDirTarget():
     label = 'ARGM-DIR'
     dataset_only_source = [
-       ('The cat jumped from the roof', 2, 5, label, 'jump.01'),
+       ('The cat jumped forward from the roof', 2, 3, label, 'jump.01'),
        ('I biked from my house', 1, 4, label, 'bike.01'),
-       ('The dog ran from the park', 2, 5, label, 'run.02'),
+       ('The dog ran along the road', 2, 5, label, 'run.02'),
     ]
 
     data_tup = add_goal(dataset_only_source, nsamples=25)
     expect_fn = Expect.pairwise(change_pred)
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-DIR-P - T1', capability='CA-DIR-P', description='X', expect=expect_fn)
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='VarDirTarget', capability='VarDirTarget', description='X', expect=expect_fn)
     return test
 
-def test_2_CA_DIR_P():
-    label = 'ARGM-DIR'
+def test_2_VarGoalSource():
+    label = 'ARGM-GOL'
     dataset_only_goal = [
-        ('Workers dumped the waste into a huge bin', 1, 6, label, 'dump.01'),
-        ('No one wants to go home', 4, 5, label, 'go.01'),
-        ('The cat jumped into the box', 2, 5, label, 'jump.01'),
-        ('The dog ran into the park', 2, 5, label, 'run.02')
+        ('Workers dumped the waste from my house', 1, -1, label, 'dump.01'),
+        ('My brother threw a paper', 2, -1, label, 'throw.01'),
+        ('The laundry should be put', 4, -1, label, 'put.01'),
     ]
 
-    data_tup = add_source(dataset_only_goal, nsamples=25)
+    data_tup = add_tar(dataset_only_goal, nsamples=25)
     expect_fn = Expect.pairwise(change_pred)
-    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='CA-DIR-P - T2', capability='CA-DIR-P', description='X', expect=expect_fn)
+    test = INV(data=data_tup, labels=[x[2] for x in data_tup], name='VarGoalSource', capability='VarGoalSource', description='X', expect=expect_fn)
     return test
 
+def add_tar(x, nsamples=10):
+    phrases = ['into a huge bin', 'into the garbage disposal', 'into the red basket', 'onto the floor']
+    cnt = 0
+    rets = []
+    while cnt < nsamples:
+        x_c = random.choice(x)
+        pred_pos = x_c[1]
+        arg_pos = x_c[2]
+        label = x_c[3]
+        pred_frame = x_c[4]
+        x_c = x_c[0]
+        p = random.choice(phrases)
+        data_tup = ('{} {}'.format(x_c, p), pred_pos, len(x_c.split(' ')) + len(p.split(' ')) - 1, label, pred_frame)
+        rets.append(data_tup)
+        cnt+=1
+
+    return rets
+
+
 def dict_fn(x):
+    #pdb.set_trace()
     sent = x[0]
-    return {'sent': sent, 'pred': sent.split(' ')[int(x[1])], 'arg_pos': int(x[2]), 'label': x[3], 'pred_frame': x[4]}
+    return {'sent': sent, 'pred': sent.split(' ')[int(x[1])], 'pred_pos': int(x[1]), 'arg_pos': int(x[2]), 'label': x[3], 'pred_frame': x[4]}
 
 def load_tests():
     suite = TestSuite()
-    suite.add(test_1_CA0_R())
-    suite.add(test_1_CA0_D())
-    suite.add(test_1_CA1_D())
-    suite.add(test_1_CA_LOC_L())
-    suite.add(test_1_CA_NEG_A())
-    suite.add(test_1_CA_TMP_F()) 
-    suite.add(test_2_CA_TMP_F()) 
-    suite.add(test_3_CA_TMP_F()) 
-    suite.add(test_1_CA_TMP_D())
-    suite.add(test_1_CA_DIR_P())
-    suite.add(test_2_CA_DIR_P())
+    suite.add(test_1_PatientRight())
+    suite.add(test_1_AgentRole3D())
+    suite.add(test_1_PatientRole3D())
+    suite.add(test_1_TempAdv())
+    suite.add(test_1_NegAdv())
+    suite.add(test_1_LocationVar())
+    suite.add(test_1_FreqAdvPast())
+    suite.add(test_2_FreqAdvPresent())
+    suite.add(test_3_FreqAdvFuture())
+    suite.add(test_1_VarDirTarget())
+    suite.add(test_2_VarGoalSource())
 
+    base_dict = suite.to_dict(dict_fn)
+    # (i) -> {'sent': sent, ... }
+    final_dict = {}
+
+    # Get the categories of the tests
+    test_names = set(base_dict['test_name'])
+    test_num = len(list(base_dict.values())[0])
+
+    for test_name in test_names:
+        test_name_list = []
+        for i in range(test_num):
+            if base_dict['test_name'][i] != test_name:
+                continue
+            current_dict = {}
+            for k in list(base_dict.keys()):
+                current_dict[k] = base_dict[k][i]
+            test_name_list.append(current_dict)
+        final_dict[test_name] = test_name_list
+
+    with open('test_data/human_readable_test_data.json', 'w') as f:
+        json.dump(final_dict, f)
     with open('test_data/raw_test_data.json', 'w') as f:
-        json.dump(suite.to_dict(dict_fn), f)
+        json.dump(base_dict, f)
+    
+
 
 
 if __name__ == '__main__':
--- Exam/utils.py
+++ Exam/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import pdb
 import re
 from checklist.perturb import process_ret, Perturb
 
@@ -12,7 +14,7 @@ def change_loc(doc, meta=False, seed=None, n=10):
     label = doc[3]
     pred_frame = doc[4]
     doc = list(nlp.pipe([doc[0]]))[0]
-    ret = Perturb.change_location(doc, meta, n=n, seed=seed)
+    ret = Perturb.change_location(doc, meta, n=n, seed=0)
     data_tup = [(x, pred_pos, arg_pos, label, pred_frame) for x in ret]
     return  data_tup
 
@@ -36,7 +38,7 @@ def perturb_time_past(doc, meta=False, seed=None, n=10):
         sub_re = re.compile(r'\b%s\b' % x)
         ret.extend([sub_re.sub(n, doc.text) for n in past])
         ret_m.extend([(x, n) for n in past])
-    data_tup = [(x, pred_pos, arg_pos, label, pred_frame) for x in ret]
+    data_tup = [(x, pred_pos, len(x.split(' '))-1, label, pred_frame) for x in ret]
     return data_tup
 
 def perturb_time_present(doc, meta=False, seed=None, n=10):
@@ -58,7 +60,7 @@ def perturb_time_present(doc, meta=False, seed=None, n=10):
         sub_re = re.compile(r'\b%s\b' % x)
         ret.extend([sub_re.sub(n, doc.text) for n in present])
         ret_m.extend([(x, n) for n in present])
-    data_tup = [(x, pred_pos, arg_pos, label, pred_frame) for x in ret]
+    data_tup = [(x, pred_pos, len(x.split(' '))-1, label, pred_frame) for x in ret]
     return data_tup
 
 def perturb_time_future(doc, meta=False, seed=None, n=10):
@@ -79,40 +81,8 @@ def perturb_time_future(doc, meta=False, seed=None, n=10):
         sub_re = re.compile(r'\b%s\b' % x)
         ret.extend([sub_re.sub(n, doc.text) for n in future])
         ret_m.extend([(x, n) for n in future])
-    data_tup = [(x, pred_pos, arg_pos, label, pred_frame) for x in ret]
+    data_tup = [(x, pred_pos, len(x.split(' '))-1, label, pred_frame) for x in ret]
     return data_tup
-
-def tokenize_and_align_labels(tokenizer, row):
-    """ Tokenize the inputs and align the labels with them. It is particularly important to
-        note the step in adding the tokenized predicate at the end of the input embeddings.
-
-   Args:
-        tokenizer: Tokenizer
-        row: dict
-    Returns:
-        dict
-
-    """
-    pred_token = row['pred'][0]
-    tok_sent = tokenizer(row["word"], is_split_into_words=True)
-    tok_whole = tokenizer(row["word"], [pred_token], padding='max_length', max_length=64, truncation=True, is_split_into_words=True)
-
-    label_ids = []
-
-    for i, word_idx in enumerate(tok_whole.word_ids()):
-        if word_idx is None:
-            # If it is a special token do not add a label
-            label_ids.append(-100)
-        elif i >= len(tok_sent['input_ids']):
-            # If the token is part of the predicate do not add a label
-            label_ids.append(-100)
-        else: 
-            # Set the label of the first token of each word
-            label_ids.append(row['label'][word_idx])
-
-
-    tok_whole["labels"] = label_ids
-    return tok_whole 
 
 def perturb_date_ref(doc, meta=False, seed=None, n=10):
     """Changes the time reference
@@ -158,7 +128,7 @@ def perturb_date_ref(doc, meta=False, seed=None, n=10):
     data_tup = [(x, pred_pos, arg_pos, label, pred_frame) for x in ret]
     return data_tup
 
-def tokenize_and_align_labels(tokenizer, row):
+def tokenize_and_align_labels_base(tokenizer, row):
     """ Tokenize the inputs and align the labels with them. It is particularly important to
         note the step in adding the tokenized predicate at the end of the input embeddings.
 
@@ -169,12 +139,14 @@ def tokenize_and_align_labels(tokenizer, row):
         dict
 
     """
-    words = row['sent'].split(' ') if type(row['sent']) is not list else row['sent']
     pred_token = row['pred']
-    tok_sent = tokenizer(words, is_split_into_words=True)
-    tok_whole = tokenizer(words, [pred_token], padding='max_length', max_length=64, truncation=True, is_split_into_words=True)
+    pred_token_base = row['pred_frame']
+    tok_sent = tokenizer(row["sent"].split(' '), is_split_into_words=True)
+    tok_whole = tokenizer(row["sent"].split(' '), [pred_token], padding='max_length', max_length=64, truncation=True, is_split_into_words=True)
 
     label_ids = []
+    pred_idx = row['pred_pos']
+    wordpiece = tokenizer.convert_ids_to_tokens(tok_whole['input_ids'])
 
     for i, word_idx in enumerate(tok_whole.word_ids()):
         if word_idx is None:
@@ -183,11 +155,97 @@ def tokenize_and_align_labels(tokenizer, row):
         elif i >= len(tok_sent['input_ids']):
             # If the token is part of the predicate do not add a label
             label_ids.append(-100)
-        elif 'arg_pos' in row and word_idx == row['arg_pos']:
-            label_ids.append(row['label'])
-        else: 
+        elif word_idx != row['arg_pos']:
             label_ids.append(-100)
+        else: 
+            label_ids.append(row['label'])
 
+    token_type_ids = torch.zeros(len(tok_whole['input_ids']))
+    scatter_idx = []
+    idx_cnt = -1
+
+    # Add token_type_ids
+    for i, word_idx in enumerate(tok_whole.word_ids()):
+        # If it is a special token do not add a label
+        if word_idx is None:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+            continue
+        elif wordpiece[i].startswith("##"):
+            scatter_idx.append(idx_cnt)
+            continue
+        elif word_idx == pred_idx:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+            token_type_ids[i] = 1
+            continue
+        else:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+
+    tok_whole["token_type_ids"] = token_type_ids.long()
     tok_whole["labels"] = label_ids
-    tok_whole['sent'] = row['sent']
+    tok_whole["scatter_idx"] = torch.tensor(scatter_idx).long()
+
+    return tok_whole 
+
+def tokenize_and_align_labels_adv(tokenizer, row):
+    """ Tokenize the inputs and align the labels with them. It is particularly important to
+        note the step in adding the tokenized predicate at the end of the input embeddings.
+
+   Args:
+        tokenizer: Tokenizer
+        row: dict
+    Returns:
+        dict
+
+    """
+    pred_token = row['pred']
+    pred_token_base = row['pred_frame']
+    tok_sent = tokenizer(row["sent"].split(' '), is_split_into_words=True)
+    tok_whole = tokenizer(row["sent"].split(' '), [pred_token, pred_token_base], padding='max_length', max_length=64, truncation=True, is_split_into_words=True)
+
+    label_ids = []
+    pred_idx = row['pred_pos']
+    wordpiece = tokenizer.convert_ids_to_tokens(tok_whole['input_ids'])
+
+    for i, word_idx in enumerate(tok_whole.word_ids()):
+        if word_idx is None:
+            # If it is a special token do not add a label
+            label_ids.append(-100)
+        elif i >= len(tok_sent['input_ids']):
+            # If the token is part of the predicate do not add a label
+            label_ids.append(-100)
+        elif word_idx != row['arg_pos']:
+            label_ids.append(-100)
+        else: 
+            # Set the label of the first token of each word
+            label_ids.append(row['label'])
+
+    token_type_ids = torch.zeros(len(tok_whole['input_ids']))
+    scatter_idx = []
+    idx_cnt = -1
+
+    # Add token_type_ids
+    for i, word_idx in enumerate(tok_whole.word_ids()):
+        # If it is a special token do not add a label
+        if word_idx is None:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+            continue
+        elif wordpiece[i].startswith("##"):
+            scatter_idx.append(idx_cnt)
+            continue
+        elif word_idx == pred_idx:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+            token_type_ids[i] = 1
+            continue
+        else:
+            idx_cnt += 1
+            scatter_idx.append(idx_cnt)
+
+    tok_whole["token_type_ids"] = token_type_ids.long()
+    tok_whole["labels"] = label_ids
+    tok_whole["scatter_idx"] = torch.tensor(scatter_idx).long()
     return tok_whole 
